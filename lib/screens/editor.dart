f83bbc490aaac3253664f7624bc22554a515ac2a
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2024-04-13 18:41:12 -0400

    fix greyed-out button

--- lib/screens/editor.dart
+++ lib/screens/editor.dart
@@ -20,7 +20,7 @@ import 'package:senior_project_hair_ai/screens/capture.dart';
 import 'package:senior_project_hair_ai/screens/settings.dart';
 import 'package:senior_project_hair_ai/screens/work.dart';
 
-void navigateToEditor(BuildContext context, {String imagePath = '', bool quietSuccess=false}) {
+void navigateToEditor(BuildContext context, {String imagePath = '', bool quietSuccess=false}) async {
   final host = Provider.of<PreferencesProvider>(context, listen: false).get<String>(apiHostPrefKey)!;
   final accessToken = Provider.of<PreferencesProvider>(context, listen: false).get<String>(apiTokenPrefKey)!;
 
@@ -34,19 +34,21 @@ void navigateToEditor(BuildContext context, {String imagePath = '', bool quietSu
   );
   //navigateTo(context: context, screen: screen, style: style)
 
-  checkAccessToken(host, accessToken, quietSuccess: quietSuccess)
-  .then((valid) {
-    Navigator.pop(context); // Pops the loading circle
-    if (valid) {
-      navigateTo(
-        context: context,
-        screen: MyEditorPage(
-          initialInputImagePath: imagePath,
-        ),
-        style: NavigationRouteStyle.material,
-      );
-    }
-  });
+  final valid = await checkAccessToken(host, accessToken, quietSuccess: quietSuccess);
+  if (!context.mounted) {
+    return;
+  }
+
+  Navigator.pop(context); // Pops the loading circle
+  if (valid) {
+    navigateTo(
+      context: context,
+      screen: MyEditorPage(
+        initialInputImagePath: imagePath,
+      ),
+      style: NavigationRouteStyle.material,
+    );
+  }
 }
 
 
@@ -68,36 +70,24 @@ class MyEditorPage extends StatefulWidget {
 
 class _MyEditorPageState extends State<MyEditorPage> {
   String _currentImagePath = '';
-  int _selectedStyleIndex = -1; // TODO should we just select index 0 for safety?
-  int _selectedColorIndex = -1;
-  late List<EditorItemModel> _selectedStyleIndexNotifiers;// = List.generate(length, (index) => null) [];
+  int _selectedStyleIndex = 0;
+  int _selectedColorIndex = 0;
+  late List<EditorItemModel> _selectedStyleIndexNotifiers;
   late List<EditorItemModel> _selectedColorIndexNotifiers;
-  //final _selectedStyleIndexProvider = StatePro
   final ValueNotifier<double> _qualityValueNotifier = ValueNotifier(1.0);
 
   late List<String> cachedTemplatesList;
 
 
   void _styleSelector(int styleIndex) {
-    //setState(() {
-
       // TODO update the list old, and new item
-    if (_selectedStyleIndex != -1) {
-      _selectedStyleIndexNotifiers[_selectedStyleIndex].update();
-    }
+    _selectedStyleIndexNotifiers[_selectedStyleIndex].update();
     _selectedStyleIndex = styleIndex;
     _selectedStyleIndexNotifiers[_selectedStyleIndex].update();
-    //});
   }
 
   void _colorSelector(int colorIndex) {
-    //setState(() {
-    // TODO test
-      //_selectedColorIndex.value = colorIndex;
-    //});
-    if (_selectedColorIndex != -1) {
-      _selectedColorIndexNotifiers[_selectedColorIndex].update();
-    }
+    _selectedColorIndexNotifiers[_selectedColorIndex].update();
     _selectedColorIndex = colorIndex;
     _selectedColorIndexNotifiers[_selectedColorIndex].update();
   }
@@ -196,7 +186,7 @@ class _MyEditorPageState extends State<MyEditorPage> {
 
           const SizedBox(height: 20),
           Text(
-            "Select Your Hairstyle: ${_selectedStyleIndex != -1 ? _selectedStyleIndex.toString() : ''}",
+            "Select Your Hairstyle:",
             style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 18.0,
@@ -267,7 +257,7 @@ class _MyEditorPageState extends State<MyEditorPage> {
             height: 20,
           ),
           Text(
-            "Select Your Hair Color: ${_selectedColorIndex != -1 ? _selectedColorIndex.toString() : ''}",
+            "Select Your Hair Color:",
             style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 18.0,
@@ -385,7 +375,7 @@ class _MyEditorPageState extends State<MyEditorPage> {
           const SizedBox(height: 20),
 
           const Text(
-            "Determine the quality (higher looks better but also much slower):",
+            "Determine the quality (the higher the quality the longer the compute time):",
             style: TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 18.0,
@@ -409,89 +399,83 @@ class _MyEditorPageState extends State<MyEditorPage> {
 
           const SizedBox(height: 20),
 
-          ElevatedButton(
-            onPressed: (_selectedColorIndex != -1 && _selectedStyleIndex != -1)
-                ? () {
-                    //if (File(path))
+           ElevatedButton(
+            onPressed: () {
+              final prefs = Provider.of<PreferencesProvider>(
+                context,
+                listen: false,
+              );
 
-                    // TODO
-                    // The API is ready here:
-                    final prefs = Provider.of<PreferencesProvider>(
-                      context,
-                      listen: false,
-                    );
+              final host = prefs.get<String>(apiHostPrefKey)!;
 
-                    final host = prefs.get<String>(apiHostPrefKey)!;
+              final styleTemplateFileName = (prefs.get<List<String>>(
+                apiCachedTemplateListPrefKey,
+              )!)[_selectedStyleIndex];
+              final colorTemplateFileName = (prefs.get<List<String>>(
+                apiCachedTemplateListPrefKey,
+              )!)[_selectedColorIndex];
 
-                    final styleTemplateFileName = (prefs.get<List<String>>(
-                      apiCachedTemplateListPrefKey,
-                    )!)[_selectedStyleIndex];
-                    final colorTemplateFileName = (prefs.get<List<String>>(
-                      apiCachedTemplateListPrefKey,
-                    )!)[_selectedColorIndex];
+              bapiApiBarberPost(
+                host: host,
+                // TODO ensure that pref defaults are loaded prior to this
+                //  SettingsPage is responsible for defaults,
+                //  why duplicate this?
+                accessToken: prefs.get<String>(apiTokenPrefKey)!,
+                // TODO determine what to do with this
+                imagePath: _currentImagePath, // require that image actually exists
+                //imageBytes: ,
+                hairStyle: styleTemplateFileName, // 'bob', // style
+                hairColor: colorTemplateFileName, // 'dark-blonde' // color
+                demo: prefs.get(apiDemoPrefKey)!,
+                quality: _qualityValueNotifier.value / 100.0
+              ).then((response) {
+                if (response.statusCode == 200) {
+                  // TODO submit to job queue
+                  final map =
+                      jsonDecode(response.body) as Map<String, dynamic>;
 
-                    bapiApiBarberPost(
-                      host: host,
-                      // TODO ensure that pref defaults are loaded prior to this
-                      //  SettingsPage is responsible for defaults,
-                      //  why duplicate this?
-                      accessToken: prefs.get<String>(apiTokenPrefKey)!,
-                      // TODO determine what to do with this
-                      imagePath: _currentImagePath, // require that image actually exists
-                      //imageBytes: ,
-                      hairStyle: styleTemplateFileName, // 'bob', // style
-                      hairColor: colorTemplateFileName, // 'dark-blonde' // color
-                      demo: prefs.get(apiDemoPrefKey)!,
-                      quality: _qualityValueNotifier.value / 100.0
-                    ).then((response) {
-                      if (response.statusCode == 200) {
-                        // TODO submit to job queue
-                        final map =
-                            jsonDecode(response.body) as Map<String, dynamic>;
+                  // submit to work queue
+                  final workID = map['work-id'] as String;
 
-                        // submit to work queue
-                        final workID = map['work-id'] as String;
+                  prefs.createListOrAdd(
+                    apiCachedWorkIDListPrefKey,
+                    [workID],
+                  );
 
-                        prefs.createListOrAdd(
-                          apiCachedWorkIDListPrefKey,
-                          [workID],
-                        );
+                  //String imageUrl = apiGeneratedUrl(host, imageName);
 
-                        //String imageUrl = apiGeneratedUrl(host, imageName);
+                  navigateTo(
+                    context: context,
+                    //screen: const MyResultsPage(),
+                    screen: MyQueuedWorkPage(),
+                    style: NavigationRouteStyle.material,
+                  );
 
-                        navigateTo(
-                          context: context,
-                          //screen: const MyResultsPage(),
-                          screen: MyQueuedWorkPage(),
-                          style: NavigationRouteStyle.material,
-                        );
+                  //Fluttertoast.showToast(msg: 'Success! $imageUrl');
+                } else if (response.statusCode == 422) {
+                  Fluttertoast.showToast(msg: 'Invalid access token');
+                } else {
+                  Fluttertoast.showToast(
+                    msg: 'Status Code: ${response.statusCode}',
+                  );
+                }
+              }).onError((error, stackTrace) {
+                log(error.toString());
+                Fluttertoast.showToast(
+                  msg: 'Failed to connect: $error',
+                );
+              });
 
-                        //Fluttertoast.showToast(msg: 'Success! $imageUrl');
-                      } else if (response.statusCode == 422) {
-                        Fluttertoast.showToast(msg: 'Invalid access token');
-                      } else {
-                        Fluttertoast.showToast(
-                          msg: 'Status Code: ${response.statusCode}',
-                        );
-                      }
-                    }).onError((error, stackTrace) {
-                      log(error.toString());
-                      Fluttertoast.showToast(
-                        msg: 'Failed to connect: $error',
-                      );
-                    });
+              // Because the AI process takes a while, use a job queue that shows submitted jobs
+              //  When the user sends to the API, the API will respond with a path of the eventual generated image
+              //  as a served file.
+              // The user can query this image path to see if the job has completed.
+              // The job queue will store a list of these job paths for the client to poll
+              //  Polling will query the server for the served image
+              // A non-200 response code means the image does not exist, or that the process is still busy
 
-                    // Because the AI process takes a while, use a job queue that shows submitted jobs
-                    //  When the user sends to the API, the API will respond with a path of the eventual generated image
-                    //  as a served file.
-                    // The user can query this image path to see if the job has completed.
-                    // The job queue will store a list of these job paths for the client to poll
-                    //  Polling will query the server for the served image
-                    // A non-200 response code means the image does not exist, or that the process is still busy
-
-                    //TODO Values to barbershop, navigate to the final screen, display results
-                  }
-                : null,
+              //TODO Values to barbershop, navigate to the final screen, display results
+            },
             child: const Text(
               'Generate',
               style: TextStyle(fontSize: 24),
